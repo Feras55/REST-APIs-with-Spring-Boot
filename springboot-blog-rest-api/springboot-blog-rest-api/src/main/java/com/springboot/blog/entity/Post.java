fb5814f8147e539b0c4ac1d7d8720fd307cb2954
FaresM55 <43585194+Feras55@users.noreply.github.com>
2022-12-20 21:05:17 +0200

    Created JPA Entity and JPA Repository
    - Learned Spring Data JPA, SpringBoot Project Structure.
    - Created Post Entity, added Lombok annotations for setters,getters and constructors
    - Created Post Repository that extends JPA Repository

---
+++ springboot-blog-rest-api/springboot-blog-rest-api/src/main/java/com/springboot/blog/entity/Post.java
@@ -0,0 +1,27 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data //Lombok.data annotation adds setters, getters and toString
+@AllArgsConstructor //generates all args constructor
+@NoArgsConstructor //generate no args constructor
+
+@Entity
+@Table(
+        name = "post", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})} //states that title is unique
+)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "content", nullable = false)
+    private String content;
+}
