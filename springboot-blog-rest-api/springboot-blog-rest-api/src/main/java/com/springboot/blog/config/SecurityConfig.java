9cdb587e96266aec85a2655e80db8f3ac758cc59
FaresM55 <43585194+Feras55@users.noreply.github.com>
2023-01-11 15:10:53 +0200

    Section 13: Adding Security to Spring Boot and Exploring Security Auto configs
    Learned more about Spring Security:
    1. spring-boot-starter-security dependency aggregates all spring security dependencies
    2. Spring boot auto configuration enables Spring Security's default configuration
    3. Spring Boot creates default user with username user and randomly generated password, and they can be customized
    4. Spring Security protects the password storage with crypt algorithm and spring boot auto configuration
    5. by default enables CSRF attack prevent
    
    - I added the spring-boot-starter-security
    - Added Security configuration

---
+++ springboot-blog-rest-api/springboot-blog-rest-api/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //Now this class becomes a Java based configuration
+public class SecurityConfig {
+    
+}
