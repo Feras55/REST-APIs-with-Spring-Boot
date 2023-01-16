7d7ac1c3cdaaa2801feb7c263d4f9092e7fe9f16
FaresM55 <43585194+Feras55@users.noreply.github.com>
2023-01-16 14:19:44 +0200

    Added Security Config

--- springboot-blog-rest-api/springboot-blog-rest-api/src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ springboot-blog-rest-api/springboot-blog-rest-api/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,8 +1,20 @@
 package com.springboot.blog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //Now this class becomes a Java based configuration
 public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                    authorize.anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
     
 }
